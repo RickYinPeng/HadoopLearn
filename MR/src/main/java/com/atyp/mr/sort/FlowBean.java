fea4a5ae1d9ddb28c15ad61e1c2f4acc24985673
RickYinPeng <272940172@qq.com>
2019-10-09 15:35:37 +0800

    全排序

---
+++ MR/src/main/java/com/atyp/mr/sort/FlowBean.java
@@ -0,0 +1,85 @@
+package com.atyp.mr.sort;
+
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class FlowBean implements WritableComparable<FlowBean> {
+    private long upFlow;    //上行流量
+    private long downFlow;  //下行流量
+    private long sumFlow;   //总流量
+
+    public FlowBean() {
+    }
+
+    public FlowBean(long upFlow, long downFlow) {
+        this.upFlow = upFlow;
+        this.downFlow = downFlow;
+        this.sumFlow = upFlow + downFlow;
+    }
+
+    // 比较
+    @Override
+    public int compareTo(FlowBean bean) {
+        // 核心比较条件判断
+        int result;
+        if (sumFlow > bean.getSumFlow()) {
+            //需要 倒序 ，所以这里是 -1
+            result = -1;
+        } else if (sumFlow < bean.getSumFlow()) {
+            result = 1;
+        } else {
+            //相等
+            result = 0;
+        }
+
+        return result;
+    }
+
+    // 序列化
+    @Override
+    public void write(DataOutput out) throws IOException {
+        out.writeLong(upFlow);
+        out.writeLong(downFlow);
+        out.writeLong(sumFlow);
+    }
+
+    // 反序列化
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        upFlow = in.readLong();
+        downFlow = in.readLong();
+        sumFlow = in.readLong();
+    }
+
+    public long getUpFlow() {
+        return upFlow;
+    }
+
+    public void setUpFlow(long upFlow) {
+        this.upFlow = upFlow;
+    }
+
+    public long getDownFlow() {
+        return downFlow;
+    }
+
+    public void setDownFlow(long downFlow) {
+        this.downFlow = downFlow;
+    }
+
+    public long getSumFlow() {
+        return sumFlow;
+    }
+
+    public void setSumFlow(long sumFlow) {
+        this.sumFlow = sumFlow;
+    }
+
+    @Override
+    public String toString() {
+        return upFlow + "\t" + downFlow + "\t" + sumFlow;
+    }
+}
